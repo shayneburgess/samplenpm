683ab0281f006618e522dc25696050a03da7a2e7
Shayne Burgess <shayne.burgess@gmail.com>
2020-04-27 15:07:31 -0700

    Initial samples

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.helloWorld = function () {
+    return 'Hello World';
+};
+//# sourceMappingURL=index.js.map
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+
+# Create a dev docker container
+
+docker run --rm -it -v $PWD:/home/app -w /home/app --name npmsample node:latest /bin/bash

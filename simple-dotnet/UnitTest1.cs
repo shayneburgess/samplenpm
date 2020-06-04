f7fb3b6c7a47ae470a25f0e34da171385a5fec1a
Jamie Cansdale <jcansdale@gmail.com>
2020-06-04 17:41:11 +0100

    Convert to console app

--- simple-dotnet/UnitTest1.cs
+++ simple-dotnet/UnitTest1.cs
@@ -1,13 +1,10 @@
-using System;
-using Xunit;
 using Newtonsoft.Json;
 
 namespace simple_dotnet
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        public static void Main()
         {
             JsonConvert.DeserializeObject<object>("{ 'hello': 'world' }");
         }
